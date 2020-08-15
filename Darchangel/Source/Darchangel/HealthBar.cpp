deed40878e83eecefa5e10ea35b86530adeac9bb
Ka Hou <0120424@kdu-online.com>
2020-08-16 00:35:45 +0800

    Delete all error/problem project

--- Darchangel/Source/Darchangel/HealthBar.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "HealthBar.h"
-
-void UHealthBar::set_bar_value_percent(float const value)
-{
-	health_value->SetPercent(value);
-}
